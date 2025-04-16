69e80e16e7e6388da5a9a5454f6b8357a01672e7
anshuls01 <singhal.s01@gmail.com>
2025-04-16 10:49:45 +0530

    Sample code to connect to mongo db
    Added Sample to connect to MongoDB, reading secret from UserSecrets

---
+++ LearnMongo/ConnectWithMongoDbInstance/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConnectWithMongoDbInstance;
+using MongoDB.Driver;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mongo Db connection");
+
+var secretAppsettingReader = new SecretAppsettingReader();
+var secretValues = secretAppsettingReader.ReadSection("mongodb");
+string mongoDbUrl = secretValues.GetSection("ConnectionString").Value;
+
+if (string.IsNullOrEmpty(mongoDbUrl))
+{ 
+    Console.WriteLine("MongoDb connection string is not set.");
+    return;
+}
+
+//Console.WriteLine($"MongoDb connection string: {mongoDbUrl}");
+var mongoClient = new MongoClient(mongoDbUrl);
+var dbList = mongoClient.ListDatabaseNames().ToList();
+Console.WriteLine("database on this server: ");
+foreach (var db in dbList)
+{ 
+   Console.WriteLine(db);
+}
---
+++ LearnMongo/ConnectWithMongoDbInstance/SecretAppsettingReader.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConnectWithMongoDbInstance
+{
+    public class SecretAppsettingReader
+    {
+        public IConfigurationSection ReadSection(string sectionName)
+        {
+            var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                //.AddJsonFile("appsettings.json")
+                //.AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets<Program>(optional: true);
+            var configurationRoot = builder.Build();
+
+            return configurationRoot.GetSection(sectionName);
+        }
+    }
+}
