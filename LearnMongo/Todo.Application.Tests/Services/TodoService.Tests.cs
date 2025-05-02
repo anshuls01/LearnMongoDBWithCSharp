83cc4e6c5c572a733e3ab12e8f5c8a78c16a60d4
anshuls01 <singhal.s01@gmail.com>
2025-05-02 06:05:11 +0530

    Sample Unit test case added for todoservice

---
+++ LearnMongo/Todo.Application.Tests/Services/TodoService.Tests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Todo.Application.DTOs;
+using Todo.Application.Interfaces;
+using Todo.Application.Services;
+using Todo.Core.Entities;
+
+namespace Todo.Application.Tests.Services
+{
+    public class TodoServiceTest
+    {
+        [Fact]
+        public async Task GetAllAsync_ReturnsListOfTodos()
+        {
+            //setup
+            var mockRepo = new Mock<ITodoRepository>();
+            mockRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<TodoItem> { new TodoItem() { IsCompleted = false, Title = "Read Book", CreatedAt = DateTime.Now, Id = 1 } });
+
+            //Act
+            var service = new TodoService(mockRepo.Object);
+            IEnumerable<TodoItemDto>? todos = await service.GetAllTodosAsync();
+
+            //Assert
+            Assert.NotNull(todos);
+            Assert.Single(todos);
+            Assert.Equal("Read Book", todos.First().Title);
+        }
+    }
+}
